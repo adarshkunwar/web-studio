91d278ac4215fa9a76d02a6d281b4135106914a9
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-31 12:03:09 +0545

    chore: set up react router dom

---
+++ src/pages/gettAllUser.tsx
@@ -0,0 +1,9 @@
+const GetAllUser = () => {
+  return (
+    <div>
+      <h1>Get All User</h1>
+    </div>
+  );
+};
+
+export default GetAllUser;
---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/signup";
+import Login from "./pages/login";
+import GetAllUser from "./pages/gettAllUser";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<GetAllUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
